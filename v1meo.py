f88480632c58ee5ec076f3f4ae6afabbbb50911d
alex <alex@safefleet.eu>
2018-03-13 14:30:43 +0200

    Improve logging

--- v1meo.py
+++ v1meo.py
@@ -13,30 +13,42 @@ class Vimeo(vimeo.VimeoClient):
             key=settings.CLIENT_ID,
             secret=settings.CLIENT_SECRET
         )
+        self.files_saved = 0
+        self.links = 0
 
     def get_data(self, videos_ids):
-        data = {}
+        logger.info("Got %s videos to proceed" % len(videos_ids))
+        data = {video_id: [] for video_id in videos_ids}
         for video_id in videos_ids:
             try:
                 response = self.get(settings.API_URL.format(video_id))
                 if response.status_code == 200:
                     picture_links = parse_response(video_id, response)
                     data[video_id] = picture_links
+                    self.links += 1
                 else:
                     logger.error("Failed to get thumbup's link for video_id=%s - %s" % (video_id, response.text))
             except Exception as e:
                 logger.error("Failed to get thumbup's link for video_id=%s - %s" % (video_id, e))
+        logger.info("Found thumbups for %s/%s videos" % (self.links, len(videos_ids)))
         return data
 
     def download_thumbups(self, data):
+        videos_with_thumbups = 0
         for video_id, links in data.items():
+            files = 0
             for i, link in enumerate(links, 1):
                 try:
                     response = self.get(link, stream=True)
                     if response.status_code == 200:
                         file_name = get_filename(video_id, i)
                         write_file(file_name, response)
+                        self.files_saved += 1
+                        files += 1
                     else:
                         logger.error('Failed to download thumbup for video %s - %s' % (video_id, response.text))
                 except Exception as e:
                     logger.error('Failed to download thumbup for video %s - %s' % (video_id, e))
+            if files > 0:
+                videos_with_thumbups += 1
+        logger.info('Downloaded %s files for %s/%s videos' % (self.files_saved, videos_with_thumbups, len(data)))
