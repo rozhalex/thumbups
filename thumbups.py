ada4ae0c88a4569a10165f5bf721d0dcbf721518
alex <alex@safefleet.eu>
2018-03-13 14:30:10 +0200

    Add option to pass video_ids as command line arguments

--- thumbups.py
+++ thumbups.py
@@ -1,9 +1,13 @@
 from utils import configure_logging
 from v1meo import Vimeo
+import sys
 
 configure_logging()
 
-video_ids = [259651498]
+if len(sys.argv[1:]) > 0:
+    video_ids = sys.argv[1:]
+else:
+    video_ids = [257287904, 174711575, 253228558]  # put video_ids here or pass them in the command line
 
 client = Vimeo()
 data = client.get_data(video_ids)
